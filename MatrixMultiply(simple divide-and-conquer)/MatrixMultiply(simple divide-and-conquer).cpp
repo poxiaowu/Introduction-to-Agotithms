9810352e13aac8bb3955c24f68f2e74551612ab4
poxiaowu <1031589204@qq.com>
2016-10-12 08:35:55 +0800

    modified

--- MatrixMultiply(simple divide-and-conquer)/MatrixMultiply(simple divide-and-conquer).cpp
+++ MatrixMultiply(simple divide-and-conquer)/MatrixMultiply(simple divide-and-conquer).cpp
@@ -1,114 +1,182 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <exception>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::cerr;
 
+// n*n matrix multiply
 struct Matrix
 {
 	Matrix() = default;
-	Matrix(const int &_row, const int &_col) : row(_row), col(_col){}
+	Matrix(const int &_row, const int &_col) : row(_row), col
+		(_col)//initial an n*n zero matrix.
+	{
+		vector<int> v(_col);
+		for (int i = 0; i != _row; i++)
+			ivec.push_back(v);
+	}
 
 	int row = 0;
 	int col = 0;
 	vector<vector<int>> ivec;
 };
 
+
 void init_matrix(Matrix &m);
+Matrix multiply_matrix(Matrix &A, Matrix &B);
+vector<Matrix> partition(Matrix &m);
 Matrix multiply_matrix_recursive(Matrix &A, Matrix &B);
-Matrix partition(Matrix &m);
+Matrix operator+(Matrix &A, Matrix &B);
 void print_matrix(Matrix &m);
+
 
 void init_matrix(Matrix &m)
 {
-	vector<int> row_v;
-
-	int ival, cnt = 0;
-	int total_num = m.row * m.col;
-	int num = 0;
+	int row = 0, col = 0;//start from first row and first col.
+	int ival;
 
 	cout << "PLZ enter a matrix(" << m.row << " * " << m.col
 		<< "):" << endl;
 
 	while (cin >> ival)
 	{
-		row_v.push_back(ival);
-		++cnt, ++num;
+		m.ivec[row][col] = ival;
+		++col;
 
-		if (cnt == m.col)
+		if (col == m.col)
 		{
-			m.ivec.push_back(row_v);
-			row_v.clear();
-			cnt = 0;
+			++row;
+			col = 0;
+			if (row == m.row)
+				break;
 		}
-
-		if (num == total_num)
-			break;
 	}
 }
 
-Matrix multiply_matrix_recursive(Matrix &A, Matrix &B)
+Matrix multiply_matrix(Matrix &A, Matrix &B)
 {
 	Matrix C(A.row, B.col);
 
 	if (A.col != B.row)
-		cout << "error: can't be multiplied!" << endl;
+		cerr << "error: can't be multiplied!" << endl;
 
 	else
 	{
-		int c_val = 0;
-		vector<int> c_row_v;
-
-		if (C.row == 1)
-		{
-			c_val = A.ivec[0][0] * B.ivec[0][0];
-			c_row_v.push_back(c_val);
-			C.ivec.push_back(c_row_v);
-			c_row_v.clear();
-		}
-		
-		else
-		{
-			int cnt = 0;
-
-			for (int i = 0; i != A.row; ++i)
-			for (int j = 0; j != B.col; ++j)
-			{
-				c_val = 0;
-				for (int k = 0; k != A.col; ++k)
-					c_val = c_val + A.ivec[i][k] * B.ivec[k][j];
-
-				c_row_v.push_back(c_val);
-				++cnt;
-
-				if (cnt == B.col)
-				{
-					C.ivec.push_back(c_row_v);
-					c_row_v.clear();
-					cnt = 0;
-				}
-			 }
-		}
-	}	
+		for (int i = 0; i != A.row; ++i)
+		for (int j = 0; j != B.col; ++j)
+		for (int k = 0; k != A.col; ++k)
+			C.ivec[i][j] += A.ivec[i][k] * B.ivec[k][j];
+	}
 	return C;
 }
 
-Matrix partition(Matrix &m)
+vector<Matrix> partition(Matrix &m)//assuming that m.row is an even.
 {
-	Matrix m_11(m.row / 2, m.col / 2);
-	Matrix m_12(m.row / 2, m.col / 2);
-	Matrix m_21(m.row / 2, m.col / 2);
-	Matrix m_22(m.row / 2, m.col / 2);
+	vector<Matrix> mvec;
 
-	vector<int> sub_v;
-	for (int i = 0; i != m.row; ++i)
+	if (m.row > 1)
 	{
-		copy_n((*(m.ivec.begin() + i)).begin(), m.col / 2, sub_v.begin());
+		Matrix part_11(m.row / 2, m.col / 2), part_12(m.row /
+			2, m.col / 2),
+			part_21(m.row / 2, m.col / 2), part_22(m.row /
+			2, m.col / 2);//four submatrices
+
+		for (int i = 0; i != m.row / 2; i++)
+		{
+			for (int j = 0; j != m.col / 2; j++)
+			{
+				part_11.ivec[i][j] = m.ivec[i][j];
+
+				part_12.ivec[i][j] = m.ivec[i][j +
+					m.col / 2];
+
+				part_21.ivec[i][j] = m.ivec[i + m.row
+					/ 2][j];
+
+				part_22.ivec[i][j] = m.ivec[i + m.row
+					/ 2][j + m.col / 2];
+			}
+		}
+
+		mvec.push_back(part_11);
+		mvec.push_back(part_12);
+		mvec.push_back(part_21);
+		mvec.push_back(part_22);
 	}
-		
+
+	else
+	{
+		mvec.push_back(m);
+	}
+
+	return mvec;
+}
+
+
+Matrix multiply_matrix_recursive(Matrix &A, Matrix &B)
+{
+	Matrix C(A.row, A.col);
+
+	if (A.row == 1 && A.col == 1)// A = B, and A is a square-matrix, that is, A.row = A.col
+	{
+		C.ivec[0][0] = A.ivec[0][0] * B.ivec[0][0];
+	}
+
+	else
+	{
+		auto A_divide = partition(A);
+		auto B_divide = partition(B);
+		auto C_divide = partition(C);
+
+		C_divide[0] = multiply_matrix_recursive(A_divide[0],
+			B_divide[0]) + multiply_matrix_recursive(A_divide[1], B_divide[2]);
+
+		C_divide[1] = multiply_matrix_recursive(A_divide[0],
+			B_divide[1]) + multiply_matrix_recursive(A_divide[1], B_divide[3]);
+
+		C_divide[2] = multiply_matrix_recursive(A_divide[2],
+			B_divide[0]) + multiply_matrix_recursive(A_divide[3], B_divide[2]);
+
+		C_divide[3] = multiply_matrix_recursive(A_divide[2],
+			B_divide[1]) + multiply_matrix_recursive(A_divide[3], B_divide[3]);
+
+		for (int i = 0; i != C.row / 2; i++)
+		for (int j = 0; j != C.col / 2; j++)
+		{
+			C.ivec[i][j] = C_divide[0].ivec[i][j];
+			C.ivec[i][j + C.col / 2] = C_divide[1].ivec
+				[i][j];
+			C.ivec[i + C.row / 2][j] = C_divide[2].ivec
+				[i][j];
+			C.ivec[i + C.row / 2][j + C.col / 2] =
+				C_divide[3].ivec[i][j];
+		}
+	}
+
+	return C;
+}
+
+Matrix operator+(Matrix &A, Matrix &B)
+{
+	Matrix C(A.row, A.col);
+
+	if (A.row == B.row && A.col == B.col)// A and B must be equal size.
+	{
+		for (int i = 0; i != A.row; ++i)
+		for (int j = 0; j != A.col; ++j)
+			C.ivec[i][j] = A.ivec[i][j] + B.ivec[i][j];
+	}
+
+	else
+	{
+		cerr << "can't add A and B." << endl;
+	}
+	return C;
 }
 
 void print_matrix(Matrix &m)
@@ -128,4 +196,28 @@ void print_matrix(Matrix &m)
 			cnt = 0;
 		}
 	}
+}
+
+
+int main()
+{
+	Matrix A(4, 4);
+	Matrix C(A.row, A.col);
+
+	init_matrix(A);
+
+
+	Matrix A_sum = A + A;
+	cout << "The sum: \n";
+	print_matrix(A_sum);
+
+	Matrix B_mul = multiply_matrix(A, A);
+	cout << "The multiply result: \n";
+	print_matrix(B_mul);
+
+	auto C_matrix = multiply_matrix_recursive(A, A);
+	print_matrix(C_matrix);
+
+	system("pause");
+	return 0;
 }
--- MatrixMultiply/MatrixMultiply.cpp
+++ MatrixMultiply/MatrixMultiply.cpp
@@ -1,64 +1,57 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <exception>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
-using std::istream;
+using std::cerr;
 
-/*****************************2016-4-20*****************************
-虽然程序看上去并不复杂，但我的实现过程并不轻松。犯得几个错误如下：
-1. vector<vector<int>> ievc;//empty vector,that is, there are no elements
-                            //in ivec! We can't use ivec[0][0]! P348
-2. if ievc is not empty,it'll provide the subscript operator.So the index
-is valid.And it's very convenient to access elements in a Sequential Container(eg.
-string, deque and array). P347
-3.auto ivec = m.ivec;//妄想使用 ivec 改变 m.ivec 的值，脑洞大开：（，跟引用有着本质的
-不同哦！
-4。限定读入多少个数，在 while(cin >> ival) 循环中直接利用某个计数值，达到要求数量直接
-break 啊!
-*******************************************************************/
-
+// n*n matrix multiply
 struct Matrix
 {
 	Matrix() = default;
-	Matrix(const int &_row, const int &_col) : row(_row), col(_col){}
+	Matrix(const int &_row, const int &_col) : row(_row), col(_col)//initial an n*n zero matrix.
+	{
+		vector<int> v(_col);
+		for (int i = 0; i != _row; i++)
+			ivec.push_back(v);
+	}
 
 	int row = 0;
 	int col = 0;
 	vector<vector<int>> ivec;
-}; 
+};
+
 
 void init_matrix(Matrix &m);
 Matrix multiply_matrix(Matrix &A, Matrix &B);
+Matrix operator+(Matrix &A, Matrix &B);
 void print_matrix(Matrix &m);
+
 
 void init_matrix(Matrix &m)
 {
-	vector<int> row_v;
+	int row = 0, col = 0;//start from first row and first col.
+	int ival;
 
-	int ival, cnt = 0;
-	int total_num = m.row * m.col;
-	int num = 0;
-	
-	cout << "PLZ enter a matrix(" << m.row << " * " << m.col 
-		 << "):" << endl;
-	
+	cout << "PLZ enter a matrix(" << m.row << " * " << m.col
+		<< "):" << endl;
+
 	while (cin >> ival)
 	{
-		row_v.push_back(ival);
-		++cnt, ++num;
+		m.ivec[row][col] = ival;
+		++col;
 
-		if (cnt == m.col)
+		if (col == m.col)
 		{
-			m.ivec.push_back(row_v);
-			row_v.clear();
-			cnt = 0;
+			++row;
+			col = 0;
+			if (row == m.row)
+				break;
 		}
-
-		if (num == total_num)
-			break;
 	}
 }
 
@@ -67,30 +60,33 @@ Matrix multiply_matrix(Matrix &A, Matrix &B)
 	Matrix C(A.row, B.col);
 
 	if (A.col != B.row)
-		cout << "error: can't be multiplied!" << endl;
-		
+		cerr << "error: can't be multiplied!" << endl;
+
 	else
 	{
-		vector<int> c_row_v;
-		int cnt = 0;
-
 		for (int i = 0; i != A.row; ++i)
 		for (int j = 0; j != B.col; ++j)
-		{
-			int c_val = 0;
-			for (int k = 0; k != A.col; ++k)
-				c_val = c_val + A.ivec[i][k] * B.ivec[k][j];
+		for (int k = 0; k != A.col; ++k)
+			C.ivec[i][j] += A.ivec[i][k] * B.ivec[k][j];
+	}
 
-			c_row_v.push_back(c_val);
-			++cnt;
+	return C;
+}
 
-			if (cnt == B.col)
-			{
-				C.ivec.push_back(c_row_v);
-				c_row_v.clear();
-				cnt = 0;
-			}
-		}
+
+Matrix operator+(Matrix &A, Matrix &B)
+{
+	Matrix C(A.row, A.col);
+	if (A.row == B.row && A.col == B.col)// A and B must be equal size.
+	{
+		for (int i = 0; i != A.row; ++i)
+		for (int j = 0; j != A.col; ++j)
+			C.ivec[i][j] = A.ivec[i][j] + B.ivec[i][j];
+	}
+
+	else
+	{
+		cerr << "can't add A and B." << endl;
 	}
 	return C;
 }
@@ -114,19 +110,22 @@ void print_matrix(Matrix &m)
 	}
 }
 
+
 int main()
 {
-	Matrix A(2, 3), B(3, 2);
+	Matrix A(4, 4);
+	Matrix C(A.row, A.col);
 
 	init_matrix(A);
-	print_matrix(A);
 
-	init_matrix(B);
-	print_matrix(B);
 
-	auto C_matrix = multiply_matrix(A, B);
+	Matrix A_sum = A + A;
+	cout << "The sum: \n";
+	print_matrix(A_sum);
 
-	print_matrix(C_matrix);
+	Matrix B_mul = multiply_matrix(A, A);
+	cout << "The multiply result: \n";
+	print_matrix(B_mul);
 
 	system("pause");
 	return 0;
